3eeea3273b18aa1e371fa0911a922922a8142c9b
Jaydip Borad <boradjaydip.bj@gmail.com>
2023-11-27 09:50:04 +0100

    pytorch file

--- app.py
+++ app.py
@@ -3,141 +3,71 @@ import streamlit as st
 import tensorflow as tf
 import pandas as pd
 import altair as alt
+import numpy as np
+from PIL import Image
+import os
+import torch
+from model import create_effnetb2_model
+
 #from utils import load_and_prep, preprocess_img, get_classes
 
-class_names = ['apple_pie',
-                'baby_back_ribs',
-                'baklava',
-                'beef_carpaccio',
-                'beef_tartare',
-                'beet_salad',
-                'beignets',
-                'bibimbap',
-                'bread_pudding',
-                'breakfast_burrito',
-                'bruschetta',
-                'caesar_salad',
-                'cannoli',
-                'caprese_salad',
-                'carrot_cake',
-                'ceviche',
-                'cheesecake',
-                'cheese_plate',
-                'chicken_curry',
-                'chicken_quesadilla',
-                'chicken_wings',
-                'chocolate_cake',
-                'chocolate_mousse',
-                'churros',
-                'clam_chowder',
-                'club_sandwich',
-                'crab_cakes',
-                'creme_brulee',
-                'croque_madame',
-                'cup_cakes',
-                'deviled_eggs',
-                'donuts',
-                'dumplings',
-                'edamame',
-                'eggs_benedict',
-                'escargots',
-                'falafel',
-                'filet_mignon',
-                'fish_and_chips',
-                'foie_gras',
-                'french_fries',
-                'french_onion_soup',
-                'french_toast',
-                'fried_calamari',
-                'fried_rice',
-                'frozen_yogurt',
-                'garlic_bread',
-                'gnocchi',
-                'greek_salad',
-                'grilled_cheese_sandwich',
-                'grilled_salmon',
-                'guacamole',
-                'gyoza',
-                'hamburger',
-                'hot_and_sour_soup',
-                'hot_dog',
-                'huevos_rancheros',
-                'hummus',
-                'ice_cream',
-                'lasagna',
-                'lobster_bisque',
-                'lobster_roll_sandwich',
-                'macaroni_and_cheese',
-                'macarons',
-                'miso_soup',
-                'mussels',
-                'nachos',
-                'omelette',
-                'onion_rings',
-                'oysters',
-                'pad_thai',
-                'paella',
-                'pancakes',
-                'panna_cotta',
-                'peking_duck',
-                'pho',
-                'pizza',
-                'pork_chop',
-                'poutine',
-                'prime_rib',
-                'pulled_pork_sandwich',
-                'ramen',
-                'ravioli',
-                'red_velvet_cake',
-                'risotto',
-                'samosa',
-                'sashimi',
-                'scallops',
-                'seaweed_salad',
-                'shrimp_and_grits',
-                'spaghetti_bolognese',
-                'spaghetti_carbonara',
-                'spring_rolls',
-                'steak',
-                'strawberry_shortcake',
-                'sushi',
-                'tacos',
-                'takoyaki',
-                'tiramisu',
-                'tuna_tartare',
-                'waffles']
+with open("class_names.txt", "r") as f:  # reading them in from class_names.txt
+    class_names = [food_name.strip() for food_name in f.readlines()]
 
-def get_classes():
-    return class_names
+# Create model
+effnetb2, effnetb2_transforms = create_effnetb2_model(num_classes=101)
 
-def preprocess_img(image):
-  image = tf.image.decode_jpeg(image, channels=3)
-  image = tf.image.convert_image_dtype(image, tf.uint8)
-  return image
 
-def load_and_prep(image, img_shape=224):
-  image = preprocess_img(image)
-  image = tf.image.resize(image, [img_shape, img_shape])
-  return tf.cast(image, tf.float32)
+# Load saved weights
+effnetb2.load_state_dict(
+    torch.load(
+        f="09_pretrained_effnetb2_feature_extractor_food101_20_percent.pth",
+	map_location=torch.device("cpu"),  # load to CPU
+
+    )
+)
+#def preprocess_img(image):
+ # image = tf.image.decode_jpeg(image, channels=3)
+  #image = tf.image.convert_image_dtype(image, tf.uint8)
+  #return image
+
+#def load_and_prep(image, img_shape=224):
+#  image = preprocess_img(image)
+#  image = tf.image.resize(image, [img_shape, img_shape])
+#  return tf.cast(image, tf.float32)
 
 #@st.cache_resource(suppress_st_warning=True)#
-def predicting(image, model):
-    image = load_and_prep(image)
-    predictions = model.predict(tf.expand_dims(image, axis=0))
-    pred_class = class_names[tf.argmax(predictions, axis=1)[0].numpy()]
-    pred_conf = tf.reduce_max(predictions[0])
-    top_5 = sorted((predictions.argsort())[0][-5:][::-1])
-    values = predictions[0][top_5] * 100
-    labels = []
-    for x in range(5):
-        labels.append(class_names[top_5[x]])
+def predicting(image):
+    #image = load_and_prep(image)
+    img = effnetb2_transforms(image).unsqueeze(0)
+    effnetb2.eval()
+    with torch.inference_mode():
+      # Pass the transformed image through the model and turn the prediction logits into prediction probabilities
+      pred_probs = torch.softmax(effnetb2(img), dim=1)
+
+    #predictions = model.predict(tf.expand_dims(image, axis=0))
+    pred_class = class_names[tf.argmax(pred_probs, axis=1)[0].numpy()]
+    pred_conf = tf.reduce_max(pred_probs[0])
+    #top_5 = sorted((predictions.argsort())[0][-5:][::-1])
+    #values = predictions[0][top_5] * 100
+    #labels = []
+    #for x in range(5):
+        #labels.append(class_names[top_5[x]])
+    
+    # Create a prediction label and prediction probability dictionary for each prediction class (this is the required format for Gradio's output parameter)
+    pred_labels_and_probs = {
+      class_names[i]: float(pred_probs[0][i]) for i in range(len(class_names))
+    }
+    values = np.array([])
+
+    labels = sorted(pred_labels_and_probs.keys(),reverse=True,key=lambda x : pred_labels_and_probs[x])[:5]    
+    for k in labels:
+      values = np.append(values, pred_labels_and_probs[k])
     df = pd.DataFrame({"Top 5 Predictions": labels,
                        "F1 Scores": values,
                        'color': ['#EC5953', '#EC5953', '#EC5953', '#EC5953', '#EC5953']})
     df = df.sort_values('F1 Scores')
     return pred_class, pred_conf, df
-
-class_names = get_classes()
 
 st.set_page_config(page_title="Food Vision",
                    page_icon="🍔")
@@ -169,7 +99,8 @@ file = st.file_uploader(label="Upload an image of food.",
                         type=["jpg", "jpeg", "png"])
 
 
-model = tf.keras.models.load_model("./07_efficientnetb0_fine_tuned_101_classes_mixed_precision_80_validation.h5")
+
+#model = tf.keras.models.load_model("./07_efficientnetb0_fine_tuned_101_classes_mixed_precision_80_validation.h5")
 
 
 st.sidebar.markdown("Created by **Jaydip Borad**")
@@ -187,12 +118,13 @@ if not file:
     st.stop()
 
 else:
-    image = file.read()
+    #image = file.read()
+    image = Image.open(file)
     st.image(image, use_column_width=True)
     pred_button = st.button("Predict")
 
 if pred_button:
-    pred_class, pred_conf, df = predicting(image, model)
+    pred_class, pred_conf, df = predicting(image)
     st.success(f'Prediction : {pred_class} \nConfidence : {pred_conf*100:.2f}%')
     st.write(alt.Chart(df).mark_bar().encode(
         x='F1 Scores',
